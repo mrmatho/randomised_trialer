3d371c373f1e4065e23fff51a523fa1890aa2517
mrmatho <111482451+mrmatho@users.noreply.github.com>
2023-09-05 12:38:21 +1000

    Update streamlit_app.py to add header and fix duplicate slider error

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import pandas as pd
 import streamlit as st
 
 import streamlit as st
+
+st.title("Randomised Trialer")
+st.header("Randomly generate your own simlations in probability", divider="rainbow")
 opts_n = st.slider("Number of Options", min_value=2, max_value=10, value=2)
 variable_prob = st.toggle('Use Variable Probabilities')
 
@@ -13,13 +16,13 @@ opts_val = []
 for i in range(1, opts_n + 1):
   opts_name.append(st.text_input(f"Option {i} Name:", value=f"Option {i}"))
   if variable_prob:
-    opts_val.append(st.slider("How many?", min_value=1, max_value=20, value=1))
+    opts_val.append(st.slider(f"How many for option {i}?", min_value=1, max_value=20, value=1))
   else:
     opts_val.append(1)
 
 pie_source = pd.DataFrame({"category": opts_name, "value": opts_val})
 
-alt.Chart(source).mark_arc().encode(
+alt.Chart(pie_source).mark_arc().encode(
     theta="value",
     color="category"
 )
