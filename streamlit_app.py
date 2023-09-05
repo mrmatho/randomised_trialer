5fafa6014243716b41b6cc41e90a08f675862f3b
mrmatho <111482451+mrmatho@users.noreply.github.com>
2023-09-06 08:23:11 +1000

    Fixed number input parameters

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ with col1:
 
 with col2:
   # n = st.slider("Number of Simulations (n):", min_value=1, max_value=10000, value=50)
-  n = st.number_input("Number of Simulations (1 - 10,000)", min = 1, max = 10000, value=50)
+  n = st.number_input("Number of Simulations (1 - 10,000)", min_value = 1, max_value = 10000, value=50)
   
 sampleNumbers = np.random.choice(opts_name, n, p=prob)
 sampleVals = sorted(dict(Counter(sampleNumbers)))
