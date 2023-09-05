a056caf763cf355ad5e2c6b37ecb100e1d00fdc2
mrmatho <111482451+mrmatho@users.noreply.github.com>
2023-09-05 12:44:55 +1000

    Moved the controls to a sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,20 +7,24 @@ import streamlit as st
 
 st.title("Randomised Trialer")
 st.header("Randomly generate your own simlations in probability", divider="rainbow")
-opts_n = st.slider("Number of Options", min_value=2, max_value=10, value=2)
-variable_prob = st.toggle('Use Variable Probabilities')
 
-opts_name = []
-opts_val = []
+with st.sidebar():
+  
+  opts_n = st.slider("Number of Options", min_value=2, max_value=10, value=2)
+  variable_prob = st.toggle('Use Variable Probabilities')
+  
+  opts_name = []
+  opts_val = []
+  
+  for i in range(1, opts_n + 1):
+    opts_name.append(st.text_input(f"Option {i} Name:", value=f"Option {i}"))
+    if variable_prob:
+      opts_val.append(st.slider(f"How many for option {i}?", min_value=1, max_value=20, value=1))
+    else:
+      opts_val.append(1)
+  
+  pie_source = pd.DataFrame({"category": opts_name, "value": opts_val})
 
-for i in range(1, opts_n + 1):
-  opts_name.append(st.text_input(f"Option {i} Name:", value=f"Option {i}"))
-  if variable_prob:
-    opts_val.append(st.slider(f"How many for option {i}?", min_value=1, max_value=20, value=1))
-  else:
-    opts_val.append(1)
-
-pie_source = pd.DataFrame({"category": opts_name, "value": opts_val})
 
 pie = alt.Chart(pie_source).mark_arc().encode(
     theta="value",
