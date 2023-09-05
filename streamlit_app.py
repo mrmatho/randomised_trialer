d476d7157cb927d93a9b7de1ca498dad0865e5e3
mrmatho <111482451+mrmatho@users.noreply.github.com>
2023-09-05 12:34:56 +1000

    Update streamlit_app.py to add in pie chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,14 @@ opts_val = []
 
 for i in range(1, opts_n + 1):
   opts_name.append(st.text_input(f"Option {i} Name:", value=f"Option {i}"))
-  
+  if variable_prob:
+    opts_val.append(st.slider("How many?", min_value=1, max_value=20, value=1))
+  else:
+    opts_val.append(1)
+
+pie_source = pd.DataFrame({"category": opts_name, "value": opts_val})
+
+alt.Chart(source).mark_arc().encode(
+    theta="value",
+    color="category"
+)
